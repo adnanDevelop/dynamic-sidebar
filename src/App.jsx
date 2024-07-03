778ed35cd35691a0a51f897bca87f473468e99b7
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-07-03 19:59:55 +0500

    completed sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
 import { CiGrid41 } from "react-icons/ci";
 import { FaRegUser, FaUsers, FaUserCheck } from "react-icons/fa";
 import { TfiWorld } from "react-icons/tfi";
 import { PiUserListDuotone } from "react-icons/pi";
-import React from "react";
 
 function App() {
+  const [activeLink, setActiveLink] = useState(0);
+
   const icons = [
     {
       icon: CiGrid41,
@@ -75,37 +77,57 @@ function App() {
   ];
 
   return (
-    <main className="relative">
+    <main className="relative flex">
       <nav className="relative h-screen w-[65px] border-r-2 border-r-[#F9F9F9] shadow-md">
         <h1 className="p-1 text-xl font-bold mb-4 text-deep-blue text-center">
           Alto
         </h1>
-        <div>
-          <ul className="flex flex-col gap-y-1 me-1">
-            {icons.map(({ icon, label }, index) => {
-              return (
-                <li key={index}>
-                  <button
-                    aria-label={label}
-                    className="w-[65px] border-r-2 text-[#222222] border-r-[#F9F9F9] h-[55px] grid place-items-center border-l-[3px] border-l-transparent hover:border-l-deep-blue cursor-pointer transitions hover:bg-[#F9F9F9] focus:bg-[#F9F9F9] bg-white"
-                  >
-                    {React.createElement(icon, {
-                      className: "text-[20px] ",
-                    })}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-
-          {/* Toolbar */}
-          <div className="fixed left-[65px] top-0 h-full bg-[#F9F9F9] w-[200px] shadow-md transitions">
-            <h2 className="p-2 font-semibold text-lg text-gray-600 text-center">
-              Team
-            </h2>
-          </div>
-        </div>
+        <ul className="flex flex-col gap-y-1 me-1">
+          {icons.map(({ icon, label }, index) => (
+            <li key={index}>
+              <button
+                aria-label={label}
+                className={`w-[65px] border-r-2 text-[#222222] border-r-[#F9F9F9] h-[55px] grid place-items-center border-l-[3px] 
+                  ${
+                    activeLink === index
+                      ? "border-l-deep-blue bg-[#F9F9F9]"
+                      : "border-l-transparent"
+                  } 
+                  hover:border-l-deep-blue cursor-pointer transitions hover:bg-[#F9F9F9] focus:bg-[#F9F9F9]`}
+                onMouseEnter={() => setActiveLink(index)}
+              >
+                {React.createElement(icon, { className: "text-[20px]" })}
+              </button>
+            </li>
+          ))}
+        </ul>
       </nav>
+
+      <div
+        className={`fixed left-[65px] top-0 h-full bg-[#F9F9F9] w-[220px] shadow-md transitions ${
+          activeLink !== null ? "block" : "hidden"
+        }`}
+      >
+        {activeLink !== null && (
+          <>
+            <h2 className="p-2 font-medium mb-4 text-lg text-gray-600 text-center">
+              {icons[activeLink].label}
+            </h2>
+            <ul className="px-4">
+              {icons[activeLink].links.map((link, index) => (
+                <li key={index} className="mb-2">
+                  <a
+                    href={link.route}
+                    className="block p-2 bg-white rounded shadow-md hover:bg-gray-200"
+                  >
+                    {link.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </main>
   );
 }
